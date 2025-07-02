6848e1833f694efd763f0f7b3383181eaa18b50d
c2y5 <97918490+c2y5@users.noreply.github.com>
2025-07-02 13:40:19 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,13 @@ git pull
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-gunicorn -b ":$PORT" "app:create_app()" --timeout 120
+
+mkdir -p logs
+
+gunicorn -b ":$PORT" "app:create_app()" \
+    --timeout 120 \
+    --access-logfile logs/app.log \
+    --access-logformat '%(h)s - - [%(t)s] "%(r)s" %(s)s -' \
+    --capture-output \
+    --error-logfile logs/error.log \
+    --log-level info
