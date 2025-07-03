1d2573d5dfa2deb192ef4d31ccc015bc9662aa38
c2y5 <97918490+c2y5@users.noreply.github.com>
2025-07-03 13:27:56 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ pip install -r requirements.txt
 gunicorn -b ":$PORT" "app:create_app()" \
   --timeout 120 \
   --access-logfile - \
-  --access-logformat '%(h)s - - [%(t)s] "%(r)s" %(s)s -' \
+  --access-logformat '%(h)s - - %(t)s "%(r)s" %(s)s -' \
   --error-logfile - \
   --capture-output 2>&1 | tee -a app.log
